19cc4430e6eba5a103ab278163309d13e0775329
Eldar <eldar.hidirow@gmail.com>
2021-01-20 20:41:44 +0300

    Lesson №49

--- js/script.js
+++ js/script.js
@@ -138,4 +138,71 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     window.addEventListener('scroll', getScrollEnd);
+    //classes
+    class Menu {
+        constructor(src, alt, title, descr, price, parent, ...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.transfer = 28.25;
+            this.convertToUAH();
+            this.parent = document.querySelector(parent);
+            this.classes = classes;
+        }
+
+        convertToUAH() {
+            this.price *= this.transfer;
+        }
+
+        rendMenu() {
+            const elem = document.createElement('div');
+
+            if(this.classes.length === 0) {
+                elem.classList.add('menu__item');
+            } else {
+                this.classes.forEach(className => elem.classList.add(className));
+            }
+
+            elem.innerHTML = `
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>`;
+            this.parent.append(elem);
+        }
+    }
+
+    new Menu(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        10,
+        '.menu .container'
+        ).rendMenu();
+
+    new Menu(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        20,
+        '.menu .container'
+        ).rendMenu();
+
+    new Menu(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков. ',
+        15,
+        '.menu .container'
+        ).rendMenu();
+
 });
